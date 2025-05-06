2d8143cb8b89a316232c93e50a3b1cf1b9020bc9
Prasanth9080 <prasanthchaandhu02@gmail.com>
2025-05-06 18:30:59 +0530

    principal dashboard desings and active and staff,student input setup

--- New_School_CRM-main/apps/staffs/models.py
+++ New_School_CRM-main/apps/staffs/models.py
@@ -76,7 +76,7 @@ WEEK_DAYS = (
 )
 
 class StaffAttendanceRecord(models.Model):
-    staff = models.ForeignKey(User, on_delete=models.CASCADE)
+    staff = models.ForeignKey(User,limit_choices_to={'userprofile__role': 'teacher'}, on_delete=models.CASCADE)
     date = models.DateField()
     month = models.CharField(max_length=20)
     day = models.CharField(max_length=10, choices=WEEK_DAYS)
